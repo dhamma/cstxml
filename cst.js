25f06e1adb011d542b668e7b10aa76b474bf2c83
yapcheahshen <yapcheahshen@gmail.com>
2013-09-27 23:16:55 +0800

    add yase build script

---
+++ cst.js
@@ -0,0 +1,22 @@
+﻿var set=process.argv[2]||'mul';
+console.log(require('yase').build({
+	dbid:'vri'+set,
+	slotshift:8,
+	loglevel:2,
+	schema:function() {
+		this.toctag(["nikaya","book"])
+			.emptytag("pb").attr("pb","n",{"depth":2,"saveval":true})
+		    .toctag("sutra").attr("sutra","id",{"depth":2,"saveval":true})
+		    .toctag("p").attr("p","n",
+		    	{"sparseval":true,"allowempty":true,"unqiue":true,"prefix":"sutra[id]","range":"-"})
+	},
+	input:set+'/vri'+set+'.lst',
+	output:'../vri'+set+'.ydb',
+	author:'yapcheahshen@gmail.com',
+	url:'http://www.ksana.tw',
+	version:'0.0.1',
+	outputencoding:'utf8',
+	//maxfile:1,
+	customfunc:require('../tools/tipitakacustom.js')
+	
+}));
